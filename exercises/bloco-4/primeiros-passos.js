4bdddb4ecbccee230c23f9e0844525b67ce12847
Daniel <danielfdoficial@gmail.com>
2021-03-25 10:55:44 -0300

    adiciona exercicios 1 a 4

---
+++ exercises/bloco-4/primeiros-passos.js
@@ -0,0 +1,44 @@
+//exercício 1
+/* let a = 10;
+let b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+ */
+//exercício 2
+/* let a = 10;
+let b = 5;
+
+if (a > b) {
+    console.log(a);
+}
+else {
+    console.log(b);
+} */
+
+//exercício 3
+/* let a = 10;
+let b = 5;
+let c = 1;
+
+if (a > b && a > c) {
+    console.log('o maior número é: ' +a+ ' (a) ');
+} else if (b > a && b > c) {
+    console.log('o maior número é: ' +b+ ' (a) ');
+} else {
+    console.log('o maior número é: ' +c+ ' (a) ');
+}
+ */
+//exercício 4
+let a = -1;
+
+if (a > 0) {
+    console.log('positive');
+} else if (a < 0) {
+    console.log('negative');
+} else {
+    console.log('zero');
+}
