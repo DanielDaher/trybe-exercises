9e820742146c1503ed55ecf3c1ea0fa5fdc3f987
Daniel <danielfdoficial@gmail.com>
2021-03-27 13:23:09 -0300

    outro modo de resolver o exercicio 8 de array e for

---
+++ exercises/bloco-4/dia-2/teste-ex-8.js
@@ -0,0 +1,9 @@
+// dica do Obama para criar um array a partir de um for;
+//exercicio 8 resolvido de outra forma;
+
+let numbers = [];
+
+for (i = 0; i < 25; i += 1) {
+    numbers[i] = i + 1;
+}
+console.log(numbers);
