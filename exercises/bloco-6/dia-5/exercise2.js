a16f2da12a9a60982f709bd55c2475e026cb9011
Daniel <danielfdoficial@gmail.com>
2021-04-26 13:52:30 -0300

    conclui exercicio da Alice Wonderland

---
+++ exercises/bloco-6/dia-5/exercise2.js
@@ -0,0 +1,8 @@
+const seta = document.querySelectorAll('.seta');
+
+for (let index = 0; index < seta.length; index +=1) {
+    seta[index].addEventListener('click', function () {
+        
+    })
+
+}
